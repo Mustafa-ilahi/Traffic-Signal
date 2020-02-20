10ebbc84ab2868ef97f6e6ae7975f5868bbd72d0
Mustafa-ilahi <mustafailahi586@gmail.com>
2020-02-21 00:29:23 +0700

    21/2/20

--- automatic.js
+++ automatic.js
@@ -1,41 +1,33 @@
-var userInput = prompt("Enter miliseconds")
-function start()
-    {
-        function red()
-        {
-            document.getElementById('red').style.backgroundColor='red';
-            document.getElementById("red").style.backgroundSize = "5px 5px";
-            setTimeout(function(){document.getElementById('red').style.backgroundColor='white' }, 1000);
-            setTimeout(function(){document.getElementById('red').style.backgroundColor='white' }, 2000);
-            setTimeout(function(){document.getElementById('red').style.backgroundColor='white' }, 3000);
-            // setTimeout(function(){document.getElementById('red').style.backgroundColor='white' }, 3000);
-        }
-        red1 = setInterval(red ,4000)
-        function yellow(){
-            document.getElementById('yellow').style.backgroundColor='white';
-            document.getElementById("red").style.backgroundSize = "5px 5px";
-            setTimeout(function(){document.getElementById('yellow').style.backgroundColor='yellow' }, 1000);
-            setTimeout(function(){document.getElementById('yellow').style.backgroundColor='white' }, 2000);
-            setTimeout(function(){document.getElementById('yellow').style.backgroundColor='white' }, 3000);
-        
-        }
-        yellow1 = setInterval(yellow ,4000)
-        function green(){
-            document.getElementById('green').style.backgroundColor='white';
-            document.getElementById("red").style.backgroundSize = "5px 5px";
-            setTimeout(function(){document.getElementById('green').style.backgroundColor='white' }, 1000);
-            setTimeout(function(){document.getElementById('green').style.backgroundColor='green' }, 2000);
-            setTimeout(function(){document.getElementById('green').style.backgroundColor='white' }, 3000);
-        }
-        green1 = setInterval(green ,4000)
-    }
-    function stop(){
-        clearInterval(red1);
-        clearInterval(yellow1);
-        clearInterval(green1);
-    }
-    function reset(){
-        document.getElementById('red').style.backgroundColor = '';
-        document.getElementById('yellow').style.backgroundColor = '';
-        document.getElementById('green').style.backgroundColor = '';
-    }
+var x,y,z;
+var userInput = prompt("Enter miliseconds");
+function start(){
+    document.getElementById("red").style.backgroundColor="red";
+    document.getElementById("red").style.backgroundSize = "5px 5px";
+    document.getElementById("yellow").style.backgroundColor="";
+    document.getElementById("green").style.backgroundColor="";
+    x=setTimeout(yellow,userInput*1000);
+}
+function yellow(){
+    document.getElementById("red").style.backgroundColor="";
+    document.getElementById("yellow").style.backgroundColor="yellow";
+    document.getElementById("yellow").style.backgroundSize = "5px 5px";
+    document.getElementById("green").style.backgroundColor="";
+    x=setTimeout(green,userInput*1000);
+}
+function green(){
+    document.getElementById("red").style.backgroundColor="";
+    document.getElementById("yellow").style.backgroundColor="";
+    document.getElementById("green").style.backgroundColor="green";
+    document.getElementById("green").style.backgroundSize = "5px 5px";
+    x=setTimeout(start,userInput*1000);
+}
+function stop(){
+    clearTimeout(x);
+    clearTimeout(y);
+    clearTimeout(z);
+}
+function reset(){
+    document.getElementById("red").style.backgroundColor="";
+    document.getElementById("yellow").style.backgroundColor="";
+    document.getElementById("green").style.backgroundColor="";
+}
